3178bee965e0aee00af1b72e7b4c47defcae6f93
arunsinghsnd <arunsinghsnd@gmail.com>
2021-02-21 21:03:42 +0530

    adding the log and header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
-
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
