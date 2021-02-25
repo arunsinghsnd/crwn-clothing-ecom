047a604ec4185f2949fce349e246d7bebe49b5cf
arunsinghsnd <arunsinghsnd@gmail.com>
2021-02-25 20:54:22 +0530

    Data Normalization + Collection

--- src/pages/collection/collection.components.js
+++ src/pages/collection/collection.components.js
@@ -4,11 +4,17 @@ import "./collection.styles.scss";
 import { connect } from "react-redux";
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
-const CollectionPage = ({ match }) => {
+const CollectionPage = ({ collection }) => {
+  const {title , items} = collection;
   return (
     <div className="collection-page">
-      <h2>Collection PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => <CollectionItem key={item.id} item={item}/>)}
+        
+      </div>
     </div>
   );
 };
