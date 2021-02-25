7cd4c1a9b41187e7f3e552687e70508c282f4886
arunsinghsnd <arunsinghsnd@gmail.com>
2021-02-25 16:33:31 +0530

    adding the cart items and removing the cart items from the checkout page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,8 +7,12 @@ import { auth } from "../../firebase/firebase-utils";
 import "./header.styles.scss";
 
 import { connect } from "react-redux";
+import { createStructuredSelector } from 'reselect';
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 const Header = ({ currentUser, hidden }) => {
   return (
@@ -40,9 +44,9 @@ const Header = ({ currentUser, hidden }) => {
   );
 };
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden,
+const mapStateToProps = createStructuredSelector({
+  currentUser:  selectCurrentUser,
+  hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);
