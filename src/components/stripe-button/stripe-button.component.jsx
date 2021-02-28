a3966272a79652fc3ad11bb14aff16a544682a9f
arunsinghsnd <arunsinghsnd@gmail.com>
2021-02-28 14:01:39 +0530

    changed the stripe button

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey =
+    "pk_test_51GrPMlA2c5eH8eVE9NXxUG19y2ekLjWqMxMeMiEQPdSvRjqzq1jMeLzwR1RFD70TU3Ig5toChqaj1luyV9UXuEoX00KZKp4vuD";
 
-  const onToken = token => {
+  const onToken = (token) => {
     console.log(token);
-    alert('Payment Succesful!');
+    alert("Payment Successful");
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      label="Pay Now"
+      name="CRWN Clothing ECOM Ltd."
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
