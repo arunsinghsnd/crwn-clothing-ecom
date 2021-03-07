36d39e909532e17742a1714784815b348badd87f
arunsinghsnd <arunsinghsnd@gmail.com>
2021-03-07 09:25:40 +0530

    maked mobile responsive

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51GrPMlA2c5eH8eVE9NXxUG19y2ekLjWqMxMeMiEQPdSvRjqzq1jMeLzwR1RFD70TU3Ig5toChqaj1luyV9UXuEoX00KZKp4vuD';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert('succesful payment');
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
